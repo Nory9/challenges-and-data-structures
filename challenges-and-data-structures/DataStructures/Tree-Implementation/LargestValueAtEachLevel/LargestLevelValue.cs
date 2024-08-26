19c45183eab3f567150180dcbb3624247d5b70a8
Nory9 <noormisk94@gmail.com>
2024-08-26 21:51:56 +0300

    cc done

---
+++ challenges-and-data-structures/DataStructures/Tree-Implementation/LargestValueAtEachLevel/LargestLevelValue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace challenges_and_data_structures.DataStructures.Tree_Implementation.LargestValueAtEachLevel
+{
+    public class LargestLevelValue : BinaryTree
+    {
+        public LargestLevelValue(int value) : base(value)
+        {
+        }
+
+        public List<int> FindLargestValuesAtEachLevel()
+        {
+            List<int> largestValues = new List<int>();
+            if (root == null)
+                return largestValues;
+
+            Queue<TNode> queue = new Queue<TNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                int levelSize = queue.Count;
+                int maxAtLevel = int.MinValue;
+
+                for (int i = 0; i < levelSize; i++)
+                {
+                    TNode currentNode = queue.Dequeue();
+                    if (currentNode.value > maxAtLevel)
+                        maxAtLevel = currentNode.value;
+
+                    if (currentNode.Left != null)
+                        queue.Enqueue(currentNode.Left);
+
+                    if (currentNode.Right != null)
+                        queue.Enqueue(currentNode.Right);
+                }
+
+                largestValues.Add(maxAtLevel);
+            }
+
+            return largestValues;
+        }
+    }
+}
